4d041e216f62d1e800f06f0dd7b4d261acf901bb
Gopal Sudarsanam <prasad.charyulu@gmail.com>
2021-07-04 12:57:41 -0700

    Updated ARm templates, parameters, script and cleanup

---
+++ scripts/gitlab-arm/gitlab-install-arm.sh
@@ -0,0 +1,29 @@
+#/bin/
+# User settings
+SUBSCRIPTION_NAME="gopal-pay-as-you-go"
+RESOURCE_GROUP_NAME="gs-rg"
+AZURE_LOCATION="westus"
+PUBLIC_KEY="$(tr -d '\n' < ~/.ssh/id_rsa.pub)"
+# ========= Do not change beyond this point =============
+# Login to Azure and capture ID of the required subscription account
+SUBSCRIPTION_ID=$(az login | jq -r --arg SUBNAME "$SUBSCRIPTION_NAME" '.[] | select( .name == $SUBNAME) | .id')
+az account set --subscription $SUBSCRIPTION_ID
+az configure --defaults group=$RESOURCE_GROUP_NAME
+az group create -l $AZURE_LOCATION -n $RESOURCE_GROUP_NAME
+# Trigger deployment on the required Resource group
+templateFile="template.json"
+ParameterFile="parameters.json"
+today=$(date +"%d-%b-%Y")
+DeploymentName="gitlab-deploy-"$today
+az deployment group create \
+ --name $DeploymentName \
+ --template-file $templateFile \
+ --parameters $ParameterFile adminPublicKey="${PUBLIC_KEY}" 
+# Check deployment status
+PROVISION_STATUS=$(az deployment group show -g $RESOURCE_GROUP_NAME -n $DeploymentName | jq -r '.properties.provisioningState')
+DEPLOY_STATUS=$(az deployment group show -g $RESOURCE_GROUP_NAME -n $DeploymentName | jq -r '.properties.error.code')
+if [[ $PROVISION_STATUS == "Failed" ]];then
+    az deployment group show -g $RESOURCE_GROUP_NAME -n $DeploymentName | jq -r '.properties.error | .code, .details[]'
+else
+    echo -e "Deployment $DeploymentName on resource Group $RESOURCE_GROUP_NAME is $PROVISION_STATUS..."
+fi
