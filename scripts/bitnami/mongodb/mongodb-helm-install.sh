edf02055a8c2e76f32b2d94897c5da48ff27388d
Gopal Sudarsanam <prasad.charyulu@gmail.com>
2021-07-07 21:34:43 -0700

    minor updates and documentation.

--- scripts/bitnami/mongodb/mongodb-helm-install.sh
+++ scripts/bitnami/mongodb/mongodb-helm-install.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#/bin/sh
 #Reference: https://docs.gitlab.com/ee/install/azure/
 #   https://docs.bitnami.com/azure/get-started-charts-marketplace/
 #   https://bitnami.com/stack/mongodb/helm
@@ -12,7 +12,7 @@ AKS_NODE_VM_SIZE="Standard_D4s_v3"
 AKS_NAMESPACE="default"
 AZURE_REGION="westus"
 PUBLIC_KEY="$(tr -d '\n' < ~/.ssh/id_rsa.pub)"
-MONGO_PUB_IP_DNS_PREFIX="${RESOURCE_GROUP_NAME}"
+MONGO_PUB_IP_DNS_PREFIX="mdb-${RESOURCE_GROUP_NAME}"
 MONGO_PUBLIC_IP_RESOURCE_NAME="mongodb-ip"
 # ========= Do not change beyond this point =============
 # Login to Azure and capture ID of the required subscription account
@@ -31,18 +31,21 @@ node_resource_group=$(az aks create \
     --name $AKS_CLUSTER \
     --node-count $AKS_NODE_COUNT \
     --node-vm-size $AKS_NODE_VM_SIZE \
-    --node-public-ip-prefix-id ${MONGO_PUB_IP_DNS_PREFIX} \
     --generate-ssh-keys | \
     jq -r '.nodeResourceGroup')
 
 echo "The node resource group created by AKS is: $node_resource_group"
 # Install Kubectl, if not available
-if which kubectl |& egrep -q ".*no kubectl.*";then
+STATUS=0
+which -s kubectl;STATUS=$?
+if [ $STATUS -eq 1 ];then
     echo -e "\n kubectl is unavailable. Installing..."
     sudo az aks install-cli
 fi
 # Install helm, if not available
-if which helm |& egrep -q ".*no helm.*";then
+STATUS=0
+which -s helm;STATUS=$?
+if [ $STATUS -eq 1 ];then
     KERNEL_NAME=$(uname -s)
     if [ "${KERNEL_NAME}" = "Darwin" ]; then
         brew install helm
@@ -60,4 +63,24 @@ az aks get-credentials -g $RESOURCE_GROUP_NAME -n $AKS_CLUSTER
 helm repo add azure-marketplace https://marketplace.azurecr.io/helm/v1/repo
 helm repo update
 helm install my-release azure-marketplace/mongodb
+# Get the Root Password
+export MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace default my-release-mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
+# Create a client container for Mongo DB
+kubectl run --namespace default my-release-mongodb-client --rm --tty -i --restart='Never' --env="MONGODB_ROOT_PASSWORD=$MONGODB_ROOT_PASSWORD" --image marketplace.azurecr.io/bitnami/mongodb:4.4.6-debian-10-r8 --command -- bash
 
+#========= IMPORTANT INFORMATION ==================
+#MongoDB can be accessed from within your cluster on the following DNS name and port (27017):
+# my-release-mongodb.default.svc.cluster.local
+
+#To get the root password run:
+# export MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace default my-release-mongodb -o jsonpath="{.data.mongodb-root-password}" | base64 --decode)
+
+#To connect to your database, create a MongoDB(R) client container:
+# kubectl run --namespace default my-release-mongodb-client --rm --tty -i --restart='Never' --env="MONGODB_ROOT_PASSWORD=$MONGODB_ROOT_PASSWORD" --image marketplace.azurecr.io/bitnami/mongodb:4.4.6-debian-10-r8 --command -- bash
+
+#Then, run the following command:
+# mongo admin --host "my-release-mongodb" --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD
+
+#To connect to your database from outside the cluster execute the following commands:
+# kubectl port-forward --namespace default svc/my-release-mongodb 27017:27017 &
+# mongo --host 127.0.0.1 --authenticationDatabase admin -p $MONGODB_ROOT_PASSWORD
